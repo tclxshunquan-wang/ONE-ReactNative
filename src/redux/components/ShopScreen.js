712a188aa146ee557452f9e9d8b4bcac276bbffc
shunq-wang <953262712@qq.com>
2017-08-11 18:33:11 +0800

    开始商城模块

--- src/redux/components/ShopScreen.js
+++ src/redux/components/ShopScreen.js
@@ -4,54 +4,98 @@ import {
     Dimensions,
     StyleSheet,
     View,
-    Easing,
-    Image
+    Text,
+    Image,
+    ScrollView,
+    TouchableOpacity
 } from 'react-native';
-import {Card, Divider} from 'react-native-elements'
+import {Divider, ListItem} from 'react-native-elements'
 import ViewPager from 'react-native-viewpager'
 const IMGS = [
-    'https://images.unsplash.com/photo-1441742917377-57f78ee0e582?h=1024',
-    'https://images.unsplash.com/photo-1441716844725-09cedc13a4e7?h=1024',
-    'https://images.unsplash.com/photo-1441448770220-76743f9e6af6?h=1024',
-    'https://images.unsplash.com/photo-1441260038675-7329ab4cc264?h=1024',
-    'https://images.unsplash.com/photo-1441126270775-739547c8680c?h=1024',
-    'https://images.unsplash.com/photo-1440964829947-ca3277bd37f8?h=1024',
-    'https://images.unsplash.com/photo-1440847899694-90043f91c7f9?h=1024'
+    'http://01.imgmini.eastday.com/mobile/20170811/20170811_ea45546c24d917e27421d6b7bfba8aac_cover_mwpm_03200403.jpeg',
+    'http://01.imgmini.eastday.com/mobile/20170811/20170811_ea45546c24d917e27421d6b7bfba8aac_cover_mwpm_03200403.jpeg',
+    'http://01.imgmini.eastday.com/mobile/20170811/20170811_ea45546c24d917e27421d6b7bfba8aac_cover_mwpm_03200403.jpeg',
 ];
 const dataSource = new ViewPager.DataSource({
     pageHasChanged: (p1, p2) => p1 !== p2,
 });
-const deviceWidth = Dimensions.get('window').width;
+const {width, height} = Dimensions.get('window');
 class ShopScreen extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this._renderPage=this._renderPage.bind(this);
-        this.state={
+        this._renderPage = this._renderPage.bind(this);
+        this.circleView=this.circleView.bind(this);
+        this.itemView=this.itemView.bind(this);
+        this.state = {
             dataSource: dataSource.cloneWithPages(IMGS),
         }
     }
 
-    _renderPage=(data,pageID)=>{
+    _renderPage = (data, pageID) => {
         return (
+            <Image
+                source={{uri: data}}
+                style={styles.page}/>
+        )
+
+    };
+
+    circleView=(obj)=>{
+        return (
+            <View  style={{flexDirection:'row',justifyContent:'space-between',padding:15}}>
+                {
+                    obj.map((item,i)=>{
+                      return (<View key={item+"_"+i}>{this.itemView(item,i)}</View>)
+                    })
+                }
+            </View>
+        )
+    };
+    itemView=(name,id)=>{
+        return (
+        <TouchableOpacity onPress={()=>alert(name)}>
+            <View style={{justifyContent:'center',alignItems:'center'}} >
                 <Image
-                    source={{uri: data}}
-                    style={styles.page} />
+                    source={{uri: IMGS[0]}}
+                    style={{height:50,width:50,borderRadius:25}}/>
+                <Text style={{fontSize:12,top:5}}>{name}</Text>
+            </View>
+        </TouchableOpacity>
         )
 
     };
 
     render() {
         return (
-            <View style={styles.itemContainer}>
-                <ViewPager
-                    style={{height:300,width:deviceWidth}}
-                    isLoop={true}
-                    autoPlay={true}
-                    dataSource={this.state.dataSource}
-                    renderPage={this._renderPage}
-                    />
+            <ScrollView>
+                <View style={styles.itemContainer}>
+                    <View style={{height:150}}>
+                        <ViewPager
+                            style={{flex:1}}
+                            isLoop={true}
+                            autoPlay={true}
+                            dataSource={this.state.dataSource}
+                            renderPage={this._renderPage}
+                        />
+                    </View>
+                    {this.circleView(['团购','活动','积分','生鲜'])}
+                    {this.circleView(['酒水','衣服','抢购','商城'])}
+                    <Divider />
+                    {
+                        [1,2,3,4,5,6,7,8,9].map((item, i) => (
+                            <ListItem
+                                roundAvatar
+                                onPress={()=>{}}
+                                key={i}
+                                title={item}
+                                underlayColor={'red'}
+                            >
+                            </ListItem>
 
-            </View>
+                        ))
+                    }
+                </View>
+            </ScrollView>
         );
     }
 }
@@ -65,8 +109,7 @@ const styles = StyleSheet.create({
         marginBottom: 5
     },
     page: {
-        width: deviceWidth,
-        height:300,
+        flex: 1
     },
 
 });
