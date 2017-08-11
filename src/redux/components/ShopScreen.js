c41ee3fa9827491b98189a754a67749429a2a604
shunq-wang <953262712@qq.com>
2017-08-11 17:23:37 +0800

    language

--- src/redux/components/ShopScreen.js
+++ src/redux/components/ShopScreen.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import {
-  AppRegistry,
-  Text,
+    Animated,
+    Dimensions,
     StyleSheet,
-  Button
+    View,
+    Easing,
+    Image
 } from 'react-native';
 import {Card, Divider} from 'react-native-elements'
+import ViewPager from 'react-native-viewpager'
+const IMGS = [
+    'https://images.unsplash.com/photo-1441742917377-57f78ee0e582?h=1024',
+    'https://images.unsplash.com/photo-1441716844725-09cedc13a4e7?h=1024',
+    'https://images.unsplash.com/photo-1441448770220-76743f9e6af6?h=1024',
+    'https://images.unsplash.com/photo-1441260038675-7329ab4cc264?h=1024',
+    'https://images.unsplash.com/photo-1441126270775-739547c8680c?h=1024',
+    'https://images.unsplash.com/photo-1440964829947-ca3277bd37f8?h=1024',
+    'https://images.unsplash.com/photo-1440847899694-90043f91c7f9?h=1024'
+];
+const dataSource = new ViewPager.DataSource({
+    pageHasChanged: (p1, p2) => p1 !== p2,
+});
+const deviceWidth = Dimensions.get('window').width;
 class ShopScreen extends React.Component {
+    constructor(props){
+        super(props);
+        this._renderPage=this._renderPage.bind(this);
+        this.state={
+            dataSource: dataSource.cloneWithPages(IMGS),
+        }
+    }
+
+    _renderPage=(data,pageID)=>{
+        return (
+                <Image
+                    source={{uri: data}}
+                    style={styles.page} />
+        )
+
+    };
 
     render() {
         return (
-            <Card
-                containerStyle={styles.card}
-                titleStyle={styles.title}
-            >
-                <Divider style={{ }} />
-            </Card>
+            <View style={styles.itemContainer}>
+                <ViewPager
+                    style={{height:300,width:deviceWidth}}
+                    isLoop={true}
+                    autoPlay={true}
+                    dataSource={this.state.dataSource}
+                    renderPage={this._renderPage}
+                    />
+
+            </View>
         );
     }
 }
@@ -23,9 +59,14 @@ class ShopScreen extends React.Component {
 const styles = StyleSheet.create({
     itemContainer: {
         backgroundColor: '#fff',
+        flex: 1
     },
     card: {
-        marginBottom:5
+        marginBottom: 5
+    },
+    page: {
+        width: deviceWidth,
+        height:300,
     },
 
 });
