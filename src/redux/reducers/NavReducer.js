bf982ce60137a438ac37b52a3503c35b521ec26e
shunq-wang <953262712@qq.com>
2017-09-26 17:37:48 +0800

    修复商城不能点击

--- src/redux/reducers/NavReducer.js
+++ src/redux/reducers/NavReducer.js
@@ -4,11 +4,6 @@ const navReducer = (state,action) => {
     let newState = Routers.router.getStateForAction(action, state)
     switch (action.type){
         case "Navigation/NAVIGATE":
-            if(action.routeName=="Shop"){
-                return {
-                    ...state
-                }
-            }
             return newState || state;
             break;
         default:
